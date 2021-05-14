d1c863367be284b2608a7827cb36477c19554019
Elton <eltongust@yahoo.com.br>
2021-05-13 21:48:33 -0300

    Exerc_aula8_mod1

---
+++ Exercícios_Aula8_Mod1.py
@@ -0,0 +1,12 @@
+L = [5, 7, 2, 9, 4, 1, 3]
+print(len(L))
+print(max(L))
+print(min(L))
+print(sum(L))
+L.reverse()
+print(L)
+L.sort()
+print (L)
+
+#Sort e Inverse não podem ser atribuídos nem embutidos no print
+#1° comnado sort depois printa Lista em ordem
---
+++ aula8.py
@@ -0,0 +1,8 @@
+lista = ["Ana", "Bernardo", "Carlos", "Daniel", "Elton", "Felipe", "Gabriel"]
+
+#PARA CADA ELEMENTO DA LISTA:
+
+for elemento in lista:
+    print(elemento)
+
+#RANGE(5) -> [0, 1, 2, 3,4]
